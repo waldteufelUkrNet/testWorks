154935ab921b4e285f06233214c56221e0be4943
waldteufelUkrNet <waldteufel@ukr.net>
2022-01-18 22:31:16 +0200

    client-structure, gulpfile

--- 20220118 Bitmedia/gulpfile.js
+++ 20220118 Bitmedia/gulpfile.js
@@ -1,9 +1,9 @@
-'use strict'
+'use strict';
 ////////////////////////////////////////////////////////////////////////////////
 /* ↓↓↓ VARIABLES ↓↓↓ */
   const { src,
           dest,
-          task,
+          // task,
           series,
           parallel,
           watch
@@ -13,30 +13,29 @@
         del     = require('del'),
         autopre = require('gulp-autoprefixer'),
         concat  = require('gulp-concat'), // ??? а css?
-        imgmin  = require('gulp-imagemin'),
+        csso    = require('gulp-csso'),
         notify  = require('gulp-notify'),
         pug     = require('gulp-pug'),
-        rename  = require('gulp-rename'),
         scss    = require('gulp-sass')(require('sass')),
         uglify  = require('gulp-uglify-es').default;
 /* ↑↑↑ /VARIABLES ↑↑↑ */
 ////////////////////////////////////////////////////////////////////////////////
-/* ↓↓↓ TASKS ↓↓↓ */
+/* ↓↓↓ TASKS (DEVELOPMENT) ↓↓↓ */
   // server for live reload
   function startBrowserSync() {
     bs.init({
       server : {
-        baseDir : 'app/client/'
+        baseDir : 'app/client/',
+        index: 'workpage.html'
       },
       notify: false // відключення повідомлень browserSync
     });
   }
-  exports.startBrowserSync = startBrowserSync;
+  module.exports.startBrowserSync = startBrowserSync;
 
-  // index.pug -> index.html
-  function convertIndexPug(){
-    return src('app/client/index.pug')
-           // .pipe(changed('app/', {extension: '.html'}))
+  // workpage: pug -> html
+  function convertWorkPage(){
+    return src('app/client/workpage.pug')
            .pipe(pug({
              pretty : true
            }))
@@ -44,14 +43,13 @@
              message : 'Error: <%= error.message %>',
              title   : 'PUG error'
            }))
-          .pipe( dest('app/client') )
+          .pipe( dest('app/client/') );
   }
-  exports.convertIndexPug = convertIndexPug;
+  module.exports.convertWorkPage = convertWorkPage;
 
-  // pug -> html
+  // pages: pug -> html
   function convertPug(){
-    return src('app/client/pug/*.pug')
-           // .pipe(changed('app/', {extension: '.html'}))
+    return src('app/client/pages/*.pug')
            .pipe(pug({
              pretty : true
            }))
@@ -59,14 +57,14 @@
              message : 'Error: <%= error.message %>',
              title   : 'PUG error'
            }))
-          .pipe( dest('app/client/html/') )
+           .pipe( dest('app/client/pages/') );
   }
-  exports.convertPug = convertPug;
+  module.exports.convertPug = convertPug;
 
   // scss -> css
   function convertSCSS() {
-    return src('app/client/scss/main.scss')
-           .pipe( scss({outputStyle: 'compressed'}) ) // nested expanded compact compressed
+    return src('app/client/scss/*.scss')
+           .pipe( scss({outputStyle: 'expanded'}) ) // nested expanded compact compressed
            .on('error', notify.onError({
               message : 'Error: <%= error.message %>',
               title   : 'SASS error'
@@ -76,88 +74,71 @@
               message : 'Error: <%= error.message %>',
               title   : 'Autoprefixer error'
             }))
-           // .pipe( rename('index.min.css') )
            .pipe( dest('app/client/css/') )
-           .pipe( bs.stream() )
+           .pipe( bs.stream() );
   }
-  exports.convertSCSS = convertSCSS;
-
-  // js modules
-  function convertModulesJS() {
-    return src('app/client/modules/**/*.js')
-           // .pipe( uglify() )
-           .on('error', notify.onError({
-              message : 'Error: <%= error.message %>',
-              title   : 'JS error'
-            }))
-           .pipe( concat('modules.js') )
-           .pipe( dest('app/client/js/') )
-           .pipe( bs.stream() )
-  }
-  exports.convertModulesJS = convertModulesJS;
-
-  // js
-  function convertJS() {
-    return src('app/client/js-expanded/*.js')
-           // .pipe( uglify() )
-           .on('error', notify.onError({
-              message : 'Error: <%= error.message %>',
-              title   : 'JS error'
-            }))
-           // .pipe( rename('index.min.js') )
-           .pipe( dest('app/client/js/') )
-           .pipe( bs.stream() )
-  }
-  exports.convertJS = convertJS;
-
-  // front to back
-  function f2b(cb) {
-    // gulp.src('app/assets/pug/*.pug').pipe(pug({pretty : false})).pipe(gulp.dest('dist/'));
-    src('app/client/html/*.html').pipe(dest('app/server/public/html/'));
-    src('app/client/css/*.css').pipe(dest('app/server/public/css/'));
-    src('app/client/fonts/**/*.*').pipe(dest('app/server/public/fonts/'));
-    src('app/client/img/**/*.*').pipe(dest('app/server/public/img/'));
-    src('app/client/js/*.js').pipe(dest('app/server/public/js/'));
-    src('app/client/libs-css/*.{css,scss}').pipe(dest('app/server/public/libs-css/'));
-    src('app/client/libs-js/*.js').pipe(dest('app/server/public/libs-js/'));
-    cb();
-  }
-  exports.f2b = f2b;
+  module.exports.convertSCSS = convertSCSS;
 
   // watching & live reload
   function startWatch(){
-    watch(['app/client/index.pug'], convertIndexPug);
-    watch(['app/client/pug/*.pug', 'app/client/modules/**/*.pug'], convertPug);
-    watch(['app/client/scss/*.scss', 'app/client/modules/**/*.scss'], convertSCSS);
-    watch(['app/client/modules/**/*.js'], convertModulesJS);
-    watch(['app/client/js-expanded/*.js'], convertJS );
-    watch(['app/client/index.html', 'app/client/html/*.html']).on('change',  bs.reload);
-
-    // copy data from client app to server app
-    watch(['app/client/html/*.html'], function f2b_html() {
-      return src('app/client/html/*.html').pipe(dest('app/server/public/html/'));
-    });
-    watch(['app/client/css/*.css'], function f2b_css() {
-      return src('app/client/css/*.css').pipe(dest('app/server/public/css/'));
-    });
-    watch(['app/client/fonts/**/*.*'], function f2b_fonts() {
-      return src('app/client/fonts/**/*.*').pipe(dest('app/server/public/fonts/'));
-    });
-    watch(['app/client/img/**/*.*'], function f2b_img() {
-      return src('app/client/img/**/*.*').pipe(dest('app/server/public/img/'));
-    });
-    watch(['app/client/js/*.js'], function f2b_js() {
-      return src('app/client/js/*.js').pipe(dest('app/server/public/js/'));
-    });
-    watch(['app/client/libs-css/*.{css,scss}'], function f2b_libs_css() {
-      return src('app/client/libs-css/*.{css,scss}').pipe(dest('app/server/public/libs-css/'));
-    });
-    watch(['app/client/libs-js/*.js'], function f2b_libs_js() {
-      return src('app/client/libs-js/*.js').pipe(dest('app/server/public/libs-js/'));
-    });
+    watch(['app/client/workpage.pug'], convertWorkPage);
+    watch(['app/client/pages/*.pug'], convertPug);
+    watch(['app/client/scss/*.scss'], convertSCSS);
+    watch(['app/client/pages/*.html', 'app/client/workpage.html']).on('change',  bs.reload);
   }
-  exports.startWatch = startWatch;
+  module.exports.startWatch = startWatch;
 
-  exports.default = series(convertSCSS, convertModulesJS, convertJS, convertPug, convertIndexPug, f2b, parallel(startBrowserSync, startWatch));
-/* ↑↑↑ /TASKS ↑↑↑ */
+  module.exports.default = series(convertSCSS, convertPug, convertWorkPage, parallel(startBrowserSync, startWatch));
+/* ↑↑↑ /TASKS (DEVELOPMENT) ↑↑↑ */
+////////////////////////////////////////////////////////////////////////////////
+/* ↓↓↓ TASKS (PRODUCTION) ↓↓↓ */
+  // function convertServerJS() {
+  //   return src(['app/*/*.js', '!app/socket/**/*'])
+  //          .pipe( uglify() )
+  //          .on('error', notify.onError({
+  //             message : 'Error: <%= error.message %>',
+  //             title   : 'JS error'
+  //           }))
+  //          .pipe( dest('dist/') )
+  // }
+  // exports.convertServerJS = convertServerJS;
+
+  // function convertAppJS() {
+  //   return src('app/app.js')
+  //          .pipe( uglify() )
+  //          .on('error', notify.onError({
+  //             message : 'Error: <%= error.message %>',
+  //             title   : 'JS error'
+  //           }))
+  //          .pipe( dest('dist/') )
+  // }
+  // exports.convertAppJS = convertAppJS;
+
+  // function copyFiles(done) {
+  //   src('app/config/config.json').pipe( dest('dist/config/') );
+  //   src('app/bin/**/*').pipe( dest('dist/bin/') );
+  //   src('app/templates/**/*.pug').pipe( dest('dist/templates/') );
+  //   src('app/datastorage/**/*').pipe( dest('dist/datastorage/') );
+  //   src('app/socket/**/*').pipe( dest('dist/socket/') );
+
+  //   src('app/public/css/main.css').pipe( csso() ).pipe( dest('dist/public/css/') );
+  //   src('app/public/fonts/**/*').pipe( dest('dist/public/fonts/') );
+  //   src('app/public/html/**/*').pipe( dest('dist/public/html/') );
+  //   src('app/public/*.html').pipe( dest('dist/public/') );
+  //   src('app/public/img/**/*').pipe( dest('dist/public/img/') );
+  //   src('app/public/js/main.js').pipe( dest('dist/public/js/') );
+
+  //   done()
+  // }
+  // exports.copyFiles = copyFiles;
+
+  // // чищення каталогу dist
+  // function clean(done) {
+  //   return del('dist');
+  //   done();
+  // }
+  // exports.clean = clean;
+
+  // exports.build = series(clean, convertServerJS, convertAppJS, copyFiles);
+/* ↑↑↑ /TASKS (PRODUCTION) ↑↑↑ */
 ////////////////////////////////////////////////////////////////////////////////
